51d35c50bed5d975dd8b474a04501ee878665e0f
Valerii Skliarov <105549682+valeriiSkliar@users.noreply.github.com>
2023-05-24 21:55:40 +0300

    init commit

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('prettier').Config}
+ */
+module.exports = {
+	trailingComma: 'all',
+	useTabs: true,
+	tabWidth: 2,
+	semi: true,
+	singleQuote: true,
+	printWidth: 120,
+	bracketSpacing: true,
+	arrowParens: 'always',
+};
