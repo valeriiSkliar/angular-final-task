15d97396d192e42710738c8f05dbc2777b679890
Valerii Skliarov <105549682+valeriiSkliar@users.noreply.github.com>
2023-05-25 09:49:07 +0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './appModuleComponents/footer/footer.component';
+import { HeaderComponent } from './appModuleComponents/header/header.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, FooterComponent, HeaderComponent],
 	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
 	providers: [],
 	bootstrap: [AppComponent],
