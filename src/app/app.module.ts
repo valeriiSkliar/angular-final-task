a651521912291e29f697b33de0eb4736b541a4f0
Valerii Skliarov <105549682+valeriiSkliar@users.noreply.github.com>
2023-05-27 22:01:22 +0300

    add notification-Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './appModuleComponents/footer/footer.component';
 import { HeaderComponent } from './appModuleComponents/header/header.component';
 import { CartModule } from './cart/cart.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
 	declarations: [AppComponent, FooterComponent, HeaderComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CartModule],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CartModule, SharedModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
