f25bda15aec17c83ea3849150efb0a44a4d5a75b
Valerii Skliarov <105549682+valeriiSkliar@users.noreply.github.com>
2023-06-10 23:13:29 +0300

    refactor function getChunkArray

--- src/app/shared/derectives/pagination/utils/getChunkArray.ts
+++ src/app/shared/derectives/pagination/utils/getChunkArray.ts
@@ -2,7 +2,6 @@ export function getChunkArray<T>(initArray: T[], chunkSize: number): T[][] {
 	let index = 0;
 	const chunksArray = [];
 
-	// eslint-disable-next-line no-cond-assign
 	for (index = 0; index < initArray.length; index += chunkSize) {
 		const chunk = initArray.slice(index, index + chunkSize);
 		chunksArray.push(chunk);
